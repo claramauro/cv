4789d0e6213f55c3f2cce39e5584bfa426b54cfa
Clara Mauro <maurog.clara@gmail.com>
2024-08-24 12:23:02 +0400

    modif header ok, sidebar en cours (todo : soft skills + interets)

--- js/app.js
+++ js/app.js
@@ -6,30 +6,6 @@ buttonsUpload.forEach(btn => {
     })
 })
 
-// Mise à jour automatique de l'âge
-
-const currentDate = new Date()
-const dayCurrentDate = currentDate.getDate()
-const monthCurrentDate = currentDate.getMonth()
-const yearCurrentDate = currentDate.getFullYear()
-
-const dayBirthday = 27
-const monthBirthday = 7 /* Aout = mois n°7 en JS Date */
-const yearBirthday = 1995
-
-let age = yearCurrentDate - yearBirthday
-if (monthCurrentDate < monthBirthday){
-    age--
-} else if (monthCurrentDate === monthBirthday  && dayCurrentDate < dayBirthday){
-    age--
-}
-
-const ageElement = document.querySelector('.js-age')
-if (parseInt(ageElement.innerText) !== age){
-    ageElement.innerText = age
-}
-
-
 
 
 
