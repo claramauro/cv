c2c1f382de9be8462e2ab0639c6d8fe0454ee3d3
Clara Mauro <maurog.clara@gmail.com>
2024-11-25 12:26:21 +0400

    modif config build vite

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/', // Ajuste les chemins pour le dossier dist
+    build: {
+        // Définit le dossier de sortie
+        outDir: 'dist',
+        // Vide le dossier de sortie avant chaque build
+        emptyOutDir: true,
+        // Génère des noms de fichiers avec un hash pour le cache
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]'
+            }
+        }
+    }
 });
